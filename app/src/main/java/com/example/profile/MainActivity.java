4bc7c3288031faf265da9b3f3f29ac1a8cb2ac09
Phan Kien <phankien110302@gmail.com>
2022-08-29 10:50:02 +0700

    add chuc nang tinh BMI

--- app/src/main/java/com/example/profile/MainActivity.java
+++ app/src/main/java/com/example/profile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.profile;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,27 +15,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void tinh_BMI(View view) {
-//        EditText txtCanNang= findViewById(R.id.can_nang);
-//        TextView textView  = findViewById(R.id.BMI);
-//        String can_nang = txtCanNang.getText().toString();
-//        EditText txtChieuCao= findViewById(R.id.chieu_cao);
-//        String chieu_cao = txtChieuCao.getText().toString();
-//        int canNang = Integer.valueOf(can_nang);
-//        int chieuCao = Integer.valueOf(chieu_cao);
-//        float BMI = canNang / (chieuCao * chieuCao);
-//        if(BMI<18)
-//            textView.setText("BMI="+BMI+"Người gầy");
-//        else
-//            if(BMI<24.9)
-//                textView.setText("BMI="+BMI+"Người bình thường");
-//        else
-//            if (BMI<29.9)
-//                textView.setText("BMI="+BMI+"Người béo phì độ I");
-//        else
-//            if(BMI <34.9)
-//                textView.setText("BMI="+BMI+"Người béo phì độ II");
-//        else
-//                textView.setText("BMI="+BMI+"Người béo phì độ III");
-//    }
+    @SuppressLint("SetTextI18n")
+    public void tinh_BMI(View view) {
+        EditText txtCanNang= findViewById(R.id.can_nang);
+        TextView textView  = findViewById(R.id.BMI);
+        EditText txtChieuCao= findViewById(R.id.chieu_cao);
+        double canNang = Double.parseDouble(txtCanNang.getText().toString());
+        double chieuCao =Double.parseDouble(txtChieuCao.getText().toString());
+        double BMI = canNang / (chieuCao * chieuCao);
+        if(BMI<18)
+            textView.setText("BMI ="+BMI+"Người gầy");
+        else
+            if(BMI<24.9)
+                textView.setText("BMI ="+BMI+ "Người bình thường");
+        else
+            if (BMI<29.9)
+                textView.setText("BMI ="+BMI+ "Người béo phì độ I");
+        else
+            if(BMI <34.9)
+                textView.setText("BMI ="+BMI+ "Người béo phì độ II");
+        else
+                textView.setText("BMI ="+BMI+ "Người béo phì độ III");
+    }
 }
